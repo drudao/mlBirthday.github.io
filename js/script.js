e1a36f629a6b72267133375a0f1e82bb0efd26c5
drudao <908561654@qq.com>
2025-08-04 19:09:18 +0800

    1
    1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // 全局变量
-const BIRTHDAY_MONTH = 8; // JavaScript中月份从0开始，所以5月是4
+const BIRTHDAY_MONTH = 7; // JavaScript中月份从0开始，所以5月是4
 const BIRTHDAY_DAY = 4;
 const CELEBRATION_DAYS = 3; // 生日庆祝持续3天
 
